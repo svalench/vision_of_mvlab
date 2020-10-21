cef9817e5ceb983487f754ee3d2852fb0df3df32
svalench <chitsalex@gmail.com>
2020-10-21 06:29:38 -0700

    authorization permissions

--- project_v_0_0_1/settings.py
+++ project_v_0_0_1/settings.py
@@ -105,8 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.BasePermission',
     ],
