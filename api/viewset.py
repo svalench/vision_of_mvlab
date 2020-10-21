5c935ac46fcf17591ad45ec4485fbcb4be08ca9a
svalench <chitsalex@gmail.com>
2020-10-21 06:29:05 -0700

    authorization permissions

--- api/viewset.py
+++ api/viewset.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from api.serializer import UserSerializer
 
-
+@permission_classes([IsAuthenticated])
 class UserList(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
