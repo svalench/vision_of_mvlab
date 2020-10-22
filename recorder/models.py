94013233f08b492b50f5035cdaac0ce9bdcdfabf
svalench <chitsalex@gmail.com>
2020-10-22 01:29:33 -0700

    описание для класса

--- recorder/models.py
+++ recorder/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 class ValueSensor(models.Model):
+    """класс предназначен для связи модели структуры Джанго и модуля сбора данных"""
     sensor = models.ForeignKey(Sensors, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default='no name')
     name_connection = models.CharField(max_length=255, default='no name connection')
@@ -18,6 +19,7 @@ class ValueSensor(models.Model):
         return self.name
 
     def get_period(self, start, end):
+        """метод возвращает данные за период start - end"""
         curs = connection.cursor()
         curs.execute(
             "SELECT * FROM `" + str(self.table_name) + "` WHERE now_time >= " +
@@ -26,10 +28,21 @@ class ValueSensor(models.Model):
         return result
 
     def get_last_shift(self):
+        """метод возвращает данные за текущую смену"""
         now = datetime.now().time().strftime('%H:%M:%S')
+        now_t = datetime.now().time()
         shifts = self.sensor.agregat.dep.shift_set.filter(start__lte=now, end__gt = now)
-        dn = datetime(now.year, now.month, now.day, shifts[0].start.hour, shifts[0].start.minute, 0)
-        de = datetime(now.year, now.month, now.day, shifts[0].end.hour, shifts[0].end.minute, 0)
+        dn = datetime(now_t.year, now_t.month, now_t.day, shifts[0].start.hour, shifts[0].start.minute, 0)
+        de = datetime(now_t.year, now_t.month, now_t.day, shifts[0].end.hour, shifts[0].end.minute, 0)
+        start = (dn - datetime(1970, 1, 1)).total_seconds()
+        end = (de - datetime(1970, 1, 1)).total_seconds()
+        return self.get_period(start=start, end=end)
+
+    def get_last_day(self):
+        """метод возвращает данные за последний день"""
+        now = datetime.now().time()
+        dn = datetime(now.year, now.month, now.day-1, now.hour, now.minute, 0)
+        de = datetime(now.year, now.month, now.day, now.hour, now.minute, 0)
         start = (dn - datetime(1970, 1, 1)).total_seconds()
         end = (de - datetime(1970, 1, 1)).total_seconds()
         return self.get_period(start=start, end=end)
