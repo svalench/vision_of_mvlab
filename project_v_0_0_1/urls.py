e6a18303f3b2e211faaeed8fe7242d38b161b527
svalench <chitsalex@gmail.com>
2020-10-26 01:49:54 -0700

    [] for urls

--- project_v_0_0_1/urls.py
+++ project_v_0_0_1/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from api.router import router
 
-urlpatterns = {
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('user/', include('users.urls')),
-}
+]
