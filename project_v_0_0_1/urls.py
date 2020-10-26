3c2091f84e20bd200469a0f117a91acd787db57c
svalench <chitsalex@gmail.com>
2020-10-26 00:33:04 -0700

    добавил путь до модуля users

--- project_v_0_0_1/urls.py
+++ project_v_0_0_1/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users import urls
 from api.router import router
 
-urlpatterns = [
+urlpatterns = {
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+    path('user/', include(urls)),
+}
