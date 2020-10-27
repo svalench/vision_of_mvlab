797f4c0af8f000e7d9cf142af6a3d7f803d04ace
svalench <chitsalex@gmail.com>
2020-10-27 04:24:58 -0700

    удалено лишнее

--- structure/viewset.py
+++ structure/viewset.py
@@ -1,11 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.decorators import permission_classes
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from .models import *
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .serializer import *
 
