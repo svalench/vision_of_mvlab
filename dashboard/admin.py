a3478b9d0be74f0b061b021fdb28258e5a8502e0
toha2 <toha2395@gmail.com>
2020-11-03 15:17:04 +0300

    registered models in admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Dashboard)
+admin.site.register(Date)
+admin.site.register(DurationIntervalDay)
+admin.site.register(RemainderStorehouse)
+admin.site.register(RemainderIso)
+admin.site.register(RemainderPol)
+admin.site.register(RemainderPen)
+admin.site.register(EditionDay)
+admin.site.register(SumexpenseDay)
+admin.site.register(EnergyConsumptionDay)
+admin.site.register(SpecificConsumptionDay)
