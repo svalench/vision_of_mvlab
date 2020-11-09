a5e0662a0f212ab6be4ab7e03c22c9fc84d1f9fd
svalench <chitsalex@gmail.com>
2020-11-09 02:25:38 -0800

    тесты для логирования

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,4 @@
-from django.test import TestCase
-from django.urls import reverse
+
 from rest_framework import status
 from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
 from rest_framework.authtoken.models import Token
