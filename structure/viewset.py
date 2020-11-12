ba5a94fb2c0b724fd8989070c7fb38e14fbfc59d
svalench <chitsalex@gmail.com>
2020-11-12 06:00:59 -0800

    методика автоподбора родителя при добавлении

--- structure/viewset.py
+++ structure/viewset.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+
 from .models import *
 from rest_framework.permissions import IsAuthenticated
 
@@ -114,6 +116,58 @@ class AgreagatView(viewsets.ModelViewSet):
         else:
             self.permission_classes = [IsAdminUser]
         return super(AgreagatView, self).get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        ob = FirstObject.objects.all().first()
+        structure = ob.listModels
+        print(request.data)
+        if 'Department' in structure:
+            pass
+        else:
+            if 'Factory' in structure:
+                try:
+                    a = Factory.objects.get(pk=request.data['parent'])
+                    request.data['parent'] = a.department_set.all().first().id
+                except:
+                    raise ValidationError('Not found Factory with pk %s'%request.data['parent'])
+            else:
+                if 'Company' in structure:
+                    try:
+                        a = Company.objects.get(pk=request.data['parent'])
+                        request.data['parent'] = a.factory_set.all().first().department_set.all().first().id
+                    except:
+                        raise ValidationError('Not found Company with pk %s' % request.data['parent'])
+                    else:
+                        if 'Corparation' in structure:
+                            try:
+                                a = Corparation.objects.get(pk=request.data['parent'])
+                                request.data['parent'] = a.company_set.all().first().factory_set.all().first().department_set.all().first().id
+                            except:
+                                raise ValidationError('Not found Corparation with pk %s' % request.data['parent'])
+                        else:
+                            if 'Reserv_2' in structure:
+                                try:
+                                    a = Reserv_2.objects.get(pk=request.data['parent'])
+                                    request.data[
+                                        'parent'] = a.corparation_set.all().first().company_set.all().first().factory_set.all().first().department_set.all().first().id
+                                except:
+                                    raise ValidationError('Not found Reserv_2 with pk %s' % request.data['parent'])
+                            else:
+                                if 'Reserv_1' in structure:
+                                    try:
+                                        a = Reserv_1.objects.get(pk=request.data['parent'])
+                                        request.data[
+                                            'parent'] = a.reserv_2_set.all().first().corparation_set.all().first().company_set.all().first().factory_set.all().first().department_set.all().first().id
+                                    except:
+                                        raise ValidationError('Not found Reserv_1 with pk %s' % request.data['parent'])
+                                else:
+                                    request.data['parent'] = Reserv_1.objects.all().firest()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 
 
