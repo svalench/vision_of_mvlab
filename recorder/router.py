5508e80c8f5a4d76bfc4bfaa737f66fc4bb8d88e
svalench <chitsalex@gmail.com>
2020-11-23 04:29:11 -0800

    пути для апи моделей

---
+++ recorder/router.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .viewset import *
+
+router_recorder = routers.DefaultRouter()
+
+
+router_recorder.register(r'Workspace', WorkspaceView)
+router_recorder.register(r'Workarea', WorkareaView)
