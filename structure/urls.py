a00b53f4b86384500bc8ff34abc1f85627e35c7e
svalench <chitsalex@gmail.com>
2020-12-02 01:14:26 -0800

    error in url settings

--- structure/urls.py
+++ structure/urls.py
@@ -5,9 +5,9 @@ from .viewset import *
 from .views import *
 
 urlpatterns = [
-    path('wizard/step1', Parametrs.step1, name='wizard_sep1'),
-    path('get_structure', Parametrs.get_structure,name='get_structure'),
-    path('create/department', Parametrs.create_shift,name='create_dep'),
+    path('wizard/step1/', Parametrs.step1, name='wizard_sep1'),
+    path('get_structure/', Parametrs.get_structure,name='get_structure'),
+    path('create/department/', Parametrs.create_shift,name='create_dep'),
 
     path( 'Reserv2/search/<int:pk>/', Reserv2_Search.as_view()),
     path( 'Corparation/search/<int:pk>/', Corparation_Search.as_view()),
