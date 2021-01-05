08ef95738b6ae1e21d08fd6f0e0d54d21203fcfb
svalench <chitsalex@gmail.com>
2021-01-05 14:54:17 +0300

    добавил модели модуля рекордер в админку

--- recorder/admin.py
+++ recorder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Workspace)
+admin.site.register(ValueSensor)
+admin.site.register(Workarea)
+admin.site.register(WorkareaData)
