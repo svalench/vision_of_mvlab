e6461d134a31aef860ca4a674caaaa3a4f263045
svalench <chitsalex@gmail.com>
2021-02-09 16:11:11 +0300

    fix raise

--- api/views_teldafax.py
+++ api/views_teldafax.py
@@ -1,3 +1,5 @@
+from rest_framework.exceptions import ValidationError
+
 from dashboard.teldefax_dashboard import TransitionReadings, GenerationOfElectricity, Status
 from rest_framework.views import APIView
 from rest_framework.response import Response
