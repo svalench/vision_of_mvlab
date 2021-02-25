9ab14926bf220fc5f6cfa32193b3f2c5b1394a36
svalench <chitsalex@gmail.com>
2021-02-25 16:06:37 +0300

    check error merge in url

--- api/urls.py
+++ api/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('teldafax/messages/alarms/', TeldafaxErrorTablesAndStatusInIt.as_view()),
     path('teldafax/status/connections/', GetStatusConnectionsTeldafax.as_view()),
     path('teldafax/connections/', GetConnectionsTeldafax.as_view()),
-    path('teldafax/connections/variables/<int:id>', GetConnectionsVariablesTeldafax.as_view()),
+    path('teldafax/connections/variables/<int:id>/', GetConnectionsVariablesTeldafax.as_view()),
     path('user/', RoleViews.as_view())
 
 ]
